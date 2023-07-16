d483e9986359cb45ba78407ca6f5aa4d8bcf1787
zhicheng <hi@unickcheng.cc>
2023-07-16 16:57:51 +0800

    ci: add semantic-release workflow

---
+++ release.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+      },
+    ],
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false,
+      },
+    ],
+    "@semantic-release/git",
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd:
+          "zip -qq -r openai-proxy-${nextRelease.version}.zip .next readme.md LICENSE package.json",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: "openai-proxy-*.zip",
+      },
+    ],
+  ],
+};
