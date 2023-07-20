de287f8e44eff65159bd183bb0458a4a022e1715
zhicheng <hi@unickcheng.cc>
2023-07-21 00:01:44 +0800

    chore(docker): change the docker-build ci

--- release.config.js
+++ release.config.js
@@ -20,22 +20,14 @@ module.exports = {
       "@semantic-release/exec",
       {
         prepareCmd:
-          "zip -qq -r openai-proxy-${nextRelease.version}.zip .next readme.md LICENSE package.json &&\
-          docker build -t unickcheng/openai-proxy:latest .",
+          "zip -qq -r openai-proxy-${nextRelease.version}.zip .next readme.md LICENSE package.json",
       },
     ],
     [
       "@semantic-release/github",
       {
-        assets: "openai-proxy-*.zip",
+        assets: "openai-proxy-*.zip", 
       },
     ],
-    [
-      "@semantic-release-plus/docker",
-      {
-        "registry": "docker.io",
-        "name": "unickcheng/openai-proxy",
-      }
-    ]
   ],
 };
